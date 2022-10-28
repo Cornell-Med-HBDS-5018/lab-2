7a0c32ac1267e3c8713e51649e96174c856ca23b
Pranav Bhimani <pranavb@berkeley.edu>
2022-10-28 17:14:51 -0400

    added function 1 code

--- R/lab2function.R
+++ R/lab2function.R
@@ -14,3 +14,29 @@ stats <- function(x){
   colnames(drg_summary)[2] <- x #change column name into corresponding calculation
   return(drg_summary) #return summary
 }
+
+
+# Function 1 code below
+## load in the libraries
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(janitor)
+library(stringr)
+library(snakecase)
+boxplotter <- function(payment_type){
+  plot <- drg_data %>%
+    clean_names() %>% # Convert names to snakecase first
+    mutate(code = str_sub(drg_definition, 1, 3)) %>% # Extract the code from DRG
+    ggplot( aes(x = code, y = get(to_snake_case(payment_type)), fill = code)) + # Create the boxplot
+    geom_boxplot() +
+    theme(axis.text.x = element_text(angle = 90)) +
+    labs(x = "DRG Code", y = payment_type,
+         title = paste0("Boxplot of ",  payment_type,
+                        " by DRG Code")) +
+    theme(legend.position = "none")
+
+  return (plot) # Output the developed boxplot
+}
+
+boxplotter('Average Covered Charges')
