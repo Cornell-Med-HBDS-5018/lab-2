86a3827a9f54419ad7fc5202c42162208cc240a0
Peizi Wu <peiwu@ucdavis.edu>
2022-10-30 23:47:07 -0400

    function 2 change

--- R/lab2function.R
+++ R/lab2function.R
@@ -1,6 +1,7 @@
 #function 2
 #Example: stats('mean')
 library(tidyverse)
+
 stats <- function(x){
   if(!is.element(x,
                  c("mean",
@@ -8,9 +9,9 @@ stats <- function(x){
                    "sd"))){
     stop("Argument statistics should be either 'mean', 'median', 'sd'.") #check if input is included in mean, median, and standard deviation
   }
-  drg_summary <- drg %>%
-    group_by(DRG.Definition) %>% #group by DRG codes
-    summarise(x = get(x)(Average.Medicare.Payments)) #calculate either the mean, median, or standard deviation
+  drg_summary <- drg_data %>%
+    group_by(`DRG Definition`) %>% #group by DRG codes
+    summarise(x = get(x)(`Average Medicare Payments`)) #calculate either the mean, median, or standard deviation
   colnames(drg_summary)[2] <- x #change column name into corresponding calculation
   return(drg_summary) #return summary
 }
